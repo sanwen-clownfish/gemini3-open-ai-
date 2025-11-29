1aca6fadcc996616d4e1bedee44561572796390e
sanwen-clownfish <109849403+sanwen-clownfish@users.noreply.github.com>
2025-11-29 10:57:43 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  }
 });
